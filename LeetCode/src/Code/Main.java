673638425b052d7dde463c1e011d692cc9dee6d0
ZypCloudy <314705487@qq.com>
2017-10-18 21:16:18 +0800

    rtoi

--- LeetCode/src/Code/Main.java
+++ LeetCode/src/Code/Main.java
@@ -5,5 +5,8 @@ public class Main {
         Solution solution = new Solution();
         Integer stoi = solution.myAtoi("  123123131");
         System.out.println(stoi);
+
+        Integer mtoi = solution.romanToInt("IV");
+        System.out.println(mtoi);
     }
 }
--- LeetCode/src/Code/Solution.java
+++ LeetCode/src/Code/Solution.java
@@ -1,8 +1,12 @@
 package Code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    //8. String to Integer (atoi)
     public int myAtoi(String str) {
-//这里要小心，需要判断有效性
+        //这里要小心，需要判断有效性
         if(str==null || str.length() == 0) {
             return 0;
         }
@@ -10,38 +14,47 @@ public class Solution {
         double sum = 0;
         int sign = 1;
         int j = 0;
-//剔除空格
+        //剔除空格
         while(str.charAt(j) == ' ')
             j++;
-//判断正数和负数
-        if(str.charAt(j) == '+')
-        {
+        //判断正数和负数
+        if(str.charAt(j) == '+') {
             sign = 1;
             j++;
-        }else if(str.charAt(j) == '-')
-        {
+        }else if(str.charAt(j) == '-') {
             sign = -1;
             j++;
         }
-        for(int i=j;i<nlen;i++)
-        {
+        for(int i=j;i<nlen;i++) {
             char current = str.charAt(i);
-            if(current >= '0' && current <= '9')
-            {
+            if(current >= '0' && current <= '9') {
                 sum = sum*10 + (int)(current - '0');
             }
-            else
-            {
+            else {
                 break;//碰到非数字，退出转换
             }
         }
-        sum = sum*sign;
-//这里要小心，需要判断范围
+        sum = sum * sign;
+        //这里要小心，需要判断范围
         if (sum > Integer.MAX_VALUE) {
             sum = Integer.MAX_VALUE;
         } else if (sum < Integer.MIN_VALUE) {
             sum = Integer.MIN_VALUE;
         }
         return (int)sum;
+    }
+    //13. Roman to Integer
+    public int romanToInt(String s) {
+        int res = 0;
+        Map map = new HashMap(){{
+            put('I',1);
+            put('V',5);
+            put('X',10);
+            put('L',50);
+            put('C',100);
+            put('D',500);
+            put('M',1000);
+        }};
+        return res;
     }
 }
