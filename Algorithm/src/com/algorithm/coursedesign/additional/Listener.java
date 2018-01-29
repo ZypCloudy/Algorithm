683b79856c4d0a9bed3dfbe0f20ebc01eaeb8237
ZypCloudy <314705487@qq.com>
2018-01-29 15:05:28 +0800

    地图着色问题

---
+++ Algorithm/src/com/algorithm/coursedesign/additional/Listener.java
@@ -0,0 +1,39 @@
+package com.algorithm.coursedesign.additional;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Listener implements ActionListener {
+    private MapColor mc;
+    private Polygon[] p;
+    private JTextField text;
+    private JCheckBox cbx;
+    private int[][] metrix;
+
+    public Listener(MapColor mc, Polygon[] p, JTextField text, JCheckBox cbx, int[][] metrix) {
+        this.mc = mc;
+        this.p = p;
+        this.text = text;
+        this.cbx = cbx;
+        this.metrix = metrix;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        Object obj = event.getSource();
+        JButton btn = (JButton) obj;
+        if (btn.getText().equals("回溯")) {
+            Recall de = new Recall(mc, cbx, metrix);
+            long startTime = System.currentTimeMillis();   //获取开始时间
+            de.backtrack();
+            long endTime = System.currentTimeMillis(); //获取结束时间
+            text.setText((endTime - startTime) + "ms");
+        }  else {
+            Recall de = new Recall(mc, cbx, metrix);
+            de.resetMap();
+            text.setText(null);
+            cbx.setSelected(false);
+        }
+    }
+}
